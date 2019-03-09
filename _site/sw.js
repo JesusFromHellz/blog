4cf098f67094f4c77247ec124128321380e42d1f
Jesus Santos <andersonp2p@hotmail.com>
2019-03-08 22:21:31 -0300

    Update files

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20190308221449";
+var CACHE_NAME = "pixyll2-20190308221951";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?201903082214",
+        "/css/pixyll.css?201903082219",
         "/"
       ]);
     })
